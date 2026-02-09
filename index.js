caa4465eebf28b967d9596f92f27cb036a9dc6e2
vexplayer1912-cyber <vexplayer1912@gmail.com>
2026-02-09 11:15:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const express = require("express");
-const app = express();
-
 const { Client, GatewayIntentBits } = require("discord.js");
 const { joinVoiceChannel } = require("@discordjs/voice");
 
-// ===== SERVIDOR HTTP (OBRIGATÓRIO NO KOYEB) =====
-app.get("/", (req, res) => {
-  res.send("Bot online 🚀");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🌐 Servidor HTTP ativo na porta ${PORT}`);
-});
-
-// ===== DISCORD BOT =====
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -26,16 +12,10 @@ client.once("ready", () => {
   console.log(`🤖 Bot ligado como ${client.user.tag}`);
 
   const guild = client.guilds.cache.get(process.env.GUILD_ID);
-  if (!guild) {
-    console.log("❌ Guild não encontrada");
-    return;
-  }
+  if (!guild) return console.log("❌ Guild não encontrada");
 
   const channel = guild.channels.cache.get(process.env.VOICE_CHANNEL_ID);
-  if (!channel) {
-    console.log("❌ Canal de voz não encontrado");
-    return;
-  }
+  if (!channel) return console.log("❌ Canal de voz não encontrado");
 
   joinVoiceChannel({
     channelId: channel.id,
