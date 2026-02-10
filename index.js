7dda54f8997cb5dd9dcefc03291c3c07445fc12d
vexplayer1912-cyber <vexplayer1912@gmail.com>
2026-02-10 10:31:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+require("libsodium-wrappers");
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.once("ready", async () => {
+  console.log(`🤖 Bot ligado como ${client.user.tag}`);
+
+  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+  if (!guild) return console.log("❌ Guild não encontrada");
+
+  const channel = guild.channels.cache.get(process.env.VOICE_CHANNEL_ID);
+  if (!channel) return console.log("❌ Canal de voz não encontrado");
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+  });
+
+  console.log("🎧 Bot entrou na call");
+});
+
+client.login(process.env.TOKEN);
